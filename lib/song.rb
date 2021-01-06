cc6aca43050311334d830b55c1675c243e112ef8
Parham Javadi <pjfooeve09@yahoo.com>
2021-01-06 13:36:51 -0800

    commit tree 67064e1d5a85ce535f4ab60c915469bfdcd13f1b

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+    attr_accessor  :artist, :name
+
+    @@all = []
+
+    def initialize (song)
+        @name = song
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artist_name
+        if !@artist
+        else
+          self.artist.name
+        end
+      end
+end
